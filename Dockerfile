9b568edfc8f92533ddda76da4f2e0aaf159c9185
Jesus Castro <tony@Jesuss-MacBook-Pro.local>
2020-09-24 02:23:24 -0700

    WIP: package json change for heroku

--- Dockerfile
+++ Dockerfile
@@ -9,11 +9,10 @@ RUN apt-get -qy update && apt-get -qy install openssl
 RUN npm install -g @prisma/cli
 RUN npm install -g @prisma/client
 
+# npm run migrate && npm run seed &
 ARG POSTGRES_URL
 ENV POSTGRES_URL "$POSTGRES_URL"
 ARG ENVIROMENT
 ENV ENVIROMENT "$ENVIROMENT"
 
-RUN npm install
-
-CMD ["sh","-c","npm run migrate && npm run seed & npm run ${ENVIROMENT}"]
+CMD ["sh","-c","npm run ${ENVIROMENT}"]
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,41 +3,35 @@ const { PrismaClient, operations } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function seed() {
-  await prisma.rate.create({
-    data: {
-      rate: 0.1,
-      type: operations.add
-    }
+  await prisma.rate.upsert({
+    where: { id: 1},
+    update: { rate: 0.1, type: operations.add },
+    create: { rate: 0.1, type: operations.add }
   });
-  await prisma.rate.create({
-    data: {
-      rate: 0.1,
-      type: operations.subtract
-    }
+  await prisma.rate.upsert({
+    where: { id: 2},
+    update: { rate: 0.1, type: operations.subtract },
+    create: { rate: 0.1, type: operations.subtract }
   });
-  await prisma.rate.create({
-    data: {
-      rate: 0.2,
-      type: operations.times
-    }
+  await prisma.rate.upsert({
+    where: { id: 3},
+    update: { rate: 0.2, type: operations.times },
+    create: { rate: 0.2, type: operations.times }
   });
-  await prisma.rate.create({
-    data: {
-      rate: 0.2,
-      type: operations.divide
-    }
+  await prisma.rate.upsert({
+    where: { id: 4},
+    update: { rate: 0.2, type: operations.divide },
+    create: { rate: 0.2, type: operations.divide }
   });
-  await prisma.rate.create({
-    data: {
-      rate: 0.5,
-      type: operations.root
-    }
+  await prisma.rate.upsert({
+    where: { id: 5},
+    update: { rate: 0.5, type: operations.root },
+    create: { rate: 0.5, type: operations.root }
   });
-  await prisma.rate.create({
-    data: {
-      rate: 0.5,
-      type: operations.random
-    }
+  await prisma.rate.upsert({
+    where: { id: 6},
+    update: { rate: 0.5, type: operations.random },
+    create: { rate: 0.5, type: operations.random }
   });
   console.log('Rate seed added::', operations);
 }
